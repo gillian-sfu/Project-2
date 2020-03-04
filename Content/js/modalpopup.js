807095893b7d888fd84c4e05ba6ebb2012bffe22
Gillian <gilliant@sfu.ca>
2020-03-03 23:10:30 -0800

    put javascript in new js tab

--- Content/js/modalpopup.js
+++ Content/js/modalpopup.js
@@ -6,16 +6,17 @@
 /*ensure that its working*/
 console.log('JavaScript is running.');
 
+	
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("review-click");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// When the user clicks the button, open the modal
 btn.onclick = function() {
 	modal.style.display = "block";
 }
@@ -29,5 +30,5 @@ span.onclick = function() {
 window.onclick = function(event) {
 	if (event.target == modal) {
 		modal.style.display = "none";
+		}
 	}
-}
